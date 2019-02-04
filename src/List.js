a44e3f98328df8a74576b38604a4440d5c0b1bdd
Owen <cyroc@dukes.jmu.edu>
2019-02-04 14:48:36 -0500

    Card component implemented

---
+++ src/List.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import './List.css';
+
+class List extends Component {
+  render() {
+    return (
+      <div class="App-list">
+      <section class="List">
+        <header class="List-header">
+          <h2>First list</h2>
+        </header>
+        <div class="List-cards">
+        </div>
+    )
+  }
+}
+
+export default List;
